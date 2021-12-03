a688d56a15d23d593bf8df8f52cee770525a3d7d
KhsirTapran <Pcboot141@gmail.com>
2021-12-03 17:24:12 +0700

    BOT COBA1

---
+++ message/mess.js
@@ -0,0 +1,17 @@
+module.exports.mess = {
+	        wait: 'PROSES SAMBIL NUNGGU... Sabar Anjg!!! @remake by kape',
+			success: 'NI HASILNYA KAK :), makaciw :* ',
+			wrongFormat: 'Format salah, coba liat lagi di menu',
+			error: {
+				api: 'APINYA HABIS BRO ISI ULANG DULU',
+				stick: 'ITU BUKAN STIKER TOLOL',
+				Iv: 'JANGAN NGASIH LINK GAJE TOD'
+			},
+			only: {
+				group: 'KHUSUS GRUP BRO',
+				admin: 'KHUSUS ADMIN GRUP BRO',
+				premium: 'LU BUKAN MEMBER PREMIUM, Bilang ke Owner dulu bro',
+				owner: 'KHUSUS OWNER BRO',
+				Badmin: 'BOT HARUS JADI ADMIN BRO',
+			}
+		}
---
+++ message/store.js
@@ -0,0 +1,62 @@
+exports.dmff = (prefix) => {
+return `
+🦴ゝLIST SERVICE DI TECHSERVICE
+ ▬▬▭▬▬▭▬▬▭▬▬▭▬▬▭▬▬
+␥COMINGSOON...
+ᗰᗰ : UPCOMING.
+ᗰᗷ : UPDATE.
+
+𖥻 Teknisi Langsung dari Ahlinya
+𖥻 Rekomendasi Konsultasi terlebih dahuluu, krn kita observasi troubel via online
+- Konsul di nomor :
+🦴 𓄼 wa.me/6283848442555
+`
+}
+exports.sewabot = (prefix) => {
+return `
+|I----Open Jasa Service----I| 
+🦴ゝLIST SERVICE DI TECHSERVICE
+ ▬▬▭▬▬▭▬▬▭▬▬▭▬▬▭▬▬
+␥COMINGSOON...
+ᗰᗰ : UPCOMING.
+ᗰᗷ : UPDATE.
+
+𖥻 Teknisi Langsung dari Ahlinya
+𖥻 Rekomendasi Konsultasi terlebih dahuluu, krn kita observasi troubel via online
+- Konsul di nomor :
+🦴 𓄼 wa.me/6283848442555
+`
+}
+exports.follig = (prefix) => {
+return `
+OPEN SUNTIK FOLL IG Khsir.Tapran
+*Cosed*
+`
+}
+exports.editing = (prefix) => {
+return `
+「JASA EDITING Video」
+*Closed Upcoming*
+`
+}
+exports.pointft = (prefix) => {
+return `
+Fitur *Closed*
+`
+}
+exports.muridbot = (prefix) => {
+return `
+OPEN MURID BUAT BOT
+*Upcoming*
+`
+}
+exports.webjualan= (prefix) => {
+return `
+*OPEN BUAT WEB UPCOMING*
+`
+}
+exports.crff = (prefix) => {
+return `
+MENU CLOSED
+`
+}
